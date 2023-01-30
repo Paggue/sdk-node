853fb32cc4d2761fd6f3f2db7cd7c0552e4a1b5a
Jimmy Lima <jimmybastosdev@gmail.com>
2023-01-30 20:52:50 -0300

    feat(cashout): oneOff example

--- src/main.ts
+++ src/main.ts
@@ -3,29 +3,49 @@ export * from './services/PaggueBaseService'
 export * from './services/PaggueBillingOrderService'
 export * from './services/PaggueCashOutService'
 
-// async function oneOff() {
-//   const logger = new LogService('oneOff')
+async function oneOff() {
+  // const logger = new LogService('OneOff')
+  //
+  // PaggueServices.init({
+  //   companyId: '203',
+  //   clientKey: '',
+  //   clientSecret: '',
+  //   signatureToken: '',
+  //   sandbox: true
+  // })
+  //
+  // const paggueCashOutService = PaggueServices.get(PaggueCashOutService)
+  //
+  // const cashout = paggueCashOutService.createTransfer({
+  //   externalId: randomUUID().toString(),
+  //   type: PaggueCashoutTypes.BankAccount,
+  //   amount: 100,
+  //   description: 'node-sdk - testing',
+  //   bankAccount: {
+  //     agency: '1488',
+  //     account: '16754',
+  //     accountDigit: '1',
+  //     accountType: 'cc',
+  //     document: '',
+  //     holder: 'Jimmy de Oliveira Bastos Lima',
+  //     ispb: '00000000'
+  //   }
+  // })
+  //
+  // logger.info('Cashout', cashout)
+  //
+  // const paggueBillingOrderService = PaggueServices.get(
+  //   PaggueBillingOrderService
+  // )
+  //
+  // const payment = await paggueBillingOrderService.createStaticPayment({
+  //   externalId: randomUUID().toString(),
+  //   amount: 2500,
+  //   description: 'New Payment',
+  //   payerName: 'Jimmy de Oliveira Bastos Lima'
+  // })
+  //
+  // logger.info('Payment', payment)
+}
 
-//   PaggueServices.init({
-//     companyId: '7114',
-//     clientKey: '7325077848134082112',
-//     clientSecret: '54748833224021937096815379',
-//     webhookToken: '51f1260d-f307-42ac-b66a-0cd48a784d3f',
-//     sandbox: false
-//   })
-
-//   const paggueBillingOrderService = PaggueServices.get(
-//     PaggueBillingOrderService
-//   )
-
-//   const payment = await paggueBillingOrderService.createStaticPayment({
-//     externalId: randomUUID().toString(),
-//     amount: 2500,
-//     description: 'New Payment',
-//     payerName: 'Jimmy de Oliveira Bastos Lima'
-//   })
-
-//   logger.info('Payment', payment)
-// }
-
-// oneOff()
+oneOff()
